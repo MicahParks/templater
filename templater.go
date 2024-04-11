a8c1fb30e3a72ac92579483a4d0ba26a381fff69
Micah Parks <66095735+MicahParks@users.noreply.github.com>
2024-04-10 20:47:57 -0400

    Add New function

--- templater.go
+++ templater.go
@@ -1,7 +1,6 @@
 package templater
 
 import (
-	"embed"
 	"fmt"
 	"html/template"
 	"io/fs"
@@ -11,6 +10,24 @@ import (
 type Templater interface {
 	Tmpl() *template.Template
 }
+
+type TemplaterOptions struct {
+	Embedded         fs.FS
+	FuncMap          template.FuncMap
+	Pattern          string
+	RootTemplateName string
+}
+
+func New(dir string, options TemplaterOptions) (Templater, error) {
+	if options.Pattern == "" {
+		options.Pattern = "*"
+	}
+	if options.Embedded != nil {
+		return NewEmbeddedTemplater(dir, options.Embedded, options.FuncMap, options.Pattern, options.RootTemplateName)
+	}
+	return NewDiskTemplater(dir, options.FuncMap, options.Pattern, options.RootTemplateName), nil
+}
+
 type DiskTemplater struct {
 	fs               fs.FS
 	funcMap          template.FuncMap
@@ -38,7 +55,7 @@ type EmbeddedTemplater struct {
 	tmpl *template.Template
 }
 
-func NewEmbeddedTemplater(dir string, embedded embed.FS, funcMap template.FuncMap, pattern, rootTemplateName string) (Templater, error) {
+func NewEmbeddedTemplater(dir string, embedded fs.FS, funcMap template.FuncMap, pattern, rootTemplateName string) (Templater, error) {
 	f, err := fs.Sub(embedded, dir)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get subdirectory of embedded file system: %w", err)
